76fa3db6bb460ee23a68202be1dac4f80c605977
stanislav.bigunenko <stas.bigunenko@gmail.com>
2022-08-30 16:12:34 +0300

    Fixed some comments

--- modules/subscriber/service/product/product.go
+++ modules/subscriber/service/product/product.go
@@ -35,7 +35,7 @@ func checkDate(input model.InputWithDate) (string, string, error) {
 	return checkFromDate.Format("2006-01-02"), checkToDate.Format("2006-01-02"), nil
 }
 
-// ActionWithInterval prepare data from the input to the needed format and send to the repositoriy
+// ActionIDWithInterval prepare data from the input to the needed format and send to the repositoriy
 func (s *ServiceProduct) ActionIDWithInterval(input model.InputWithDate) ([]model.DBResponse, error) {
 	fromDate, toDate, err := checkDate(input)
 	if err != nil {
@@ -50,7 +50,7 @@ func (s *ServiceProduct) ActionIDWithInterval(input model.InputWithDate) ([]mode
 	return result, nil
 }
 
-// ActionWithInterval prepare data from the input to the needed format and send to the repositoriy
+// TwoActionsIDWithInterval prepare data from the input to the needed format and send to the repositoriy
 func (s *ServiceProduct) TwoActionsIDWithInterval(input model.InputWithDate) ([]model.DBResponse2Actions, error) {
 	fromDate, toDate, err := checkDate(input)
 	if err != nil {
